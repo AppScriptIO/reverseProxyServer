b7815c9a342263993af0f3aedaa0f4f589e359dd
Safi <sfmissive@gmail.com>
2019-05-07 21:20:07 +0300

    ⚗ Refactor to allow manual building and releasing of package.

--- configuration/babel.config.js
+++ configuration/babel.config.js
@@ -1,3 +1,4 @@
-const { getBabelConfig } = require('@dependency/javascriptTranspilation')
+const configuration = require('./'),
+  { getBabelConfig } = require('@dependency/javascriptTranspilation')
 
-module.exports = getBabelConfig('serverRuntime.BabelConfig.js', { configType: 'functionApi' })
+module.exports = getBabelConfig(configuration.transpilation.babelConfigKey, { configType: 'functionApi' })
